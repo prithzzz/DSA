b3da23d7e9726bad32bef317bc42a23019304b53
Prithika Soundararajan <prithikasoundararajan@gmail.com>
2026-07-06 14:33:03 +0530

    Time: 1 ms (99.94%) | Memory: 94.6 MB (15.38%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,19 @@
+// Approach: (array 2 pointers)
+// Assume 2 index pointer buy=0 and sell=1. Compare if sell price is greater than buy price, then find profit and compare with the existing max profit.
+// Else if ant sell price is lesser than buy price, then that sell becomes the new buy price.
+
+
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int buy = 0; int sell = 1;
         int profit = 0;
         while(buy<n && sell<n){
-            if(prices[buy] < prices[sell]){
+            if(prices[buy] < prices[sell])
                 profit = Math.max(profit, prices[sell]-prices[buy]);
-            }
             else
                 buy = sell;
+                
             sell++;
         }
         return profit;

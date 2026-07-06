5e9ea7d81ee607359802c1849ed16ff49c561f79
prithzzz <prithikasoundararajan@gmail.com>
2026-07-06 20:57:19 +0530

    Organised by data structure

--- Array (2 pointers)/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ Array (2 pointers)/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,3 +1,13 @@
+// Approach 1: (array 2 pointers)
+// Use two pointer i,j to comapre the absolute value of first and last elements (moving inward).
+// Place the square of the larger value to the end of result array. Return result array.
+// Time complexity - O(n)
+
+// Approach 2: (Brute force)
+// Square every element in the array and then sort.
+// Time complexity - O(n.log n)
+
+
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;

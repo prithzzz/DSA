c197bc67861a925ba49c84d356374ef35d6c9f3e
Prithika Soundararajan <prithikasoundararajan@gmail.com>
2026-07-07 22:24:27 +0530

    Time: 27 ms (98.92%) | Memory: 59.4 MB (32.89%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,3 +1,11 @@
+// Approach: (array 2 pointers)
+// Brute force method will have time complexity O(n^3)
+// So, sort the array, and assume k=0 and two pointers i=k+1, j=n-1.
+// Add nums at i,j,k
+// If sum is >0, then move i. If sum is <0 then move j.
+// Check if i or k repeat with the adjacent value in array (otherwise will lead to duplicate results)
+
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;

7634f02ab1905d2d8fe3fccff35f0e5a8a6a5c9c
Prithika Soundararajan <prithikasoundararajan@gmail.com>
2026-07-08 12:50:45 +0530

    Time: 2 ms (99.66%) | Memory: 47.6 MB (6.57%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.java
+++ 875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -1,3 +1,9 @@
+// Approach: (array 2 pointers)
+// Start with the element at index i=1, and find i such that left and right elements are less than i.
+// Then iterate pointer l such that all elements to its left are lesser and interate pointer r such that all elements to its right are lesser.
+// Find r-l+1 and compare with prev count to find the max length of the mountain.
+
+
 class Solution {
     public int longestMountain(int[] arr) {
         int n = arr.length;

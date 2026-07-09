8193a61e6eb71a9d1970747caa9cadef2dd8c9ec
Prithika Soundararajan <prithikasoundararajan@gmail.com>
2026-07-09 20:36:56 +0530

    Time: 23 ms (79.01%) | Memory: 90.8 MB (28.20%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,3 +1,8 @@
+// Approach: (Sliding window)
+// for every new element, add it into a visited hashmap to along with its index. Check if the elements already exists in the map.
+// To check if it exists within given window, use the current index of duplicate element in array and the index of the previous duplicate stored in the hashmap.
+
+
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         int n = nums.length;

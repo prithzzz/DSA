caa6a1fa2faece0a27af20479f33022e44fe701f
Prithika Soundararajan <prithikasoundararajan@gmail.com>
2026-07-13 21:04:32 +0530

    Time: 15 ms (82.14%) | Memory: 46.2 MB (72.27%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,7 +1,7 @@
 // Approach: (Knapsack-type problem)
 // Compute the min number of coins required for every amount value from 0 to amount. Initially fill with value(amount+1). 
 // Calculate for each amount value-
-// dp[i] = min(dp[i], 1+dp[i-c]) 
+// dp[i] = min(dp[i], 1+dp[i-c]) , [refer img]
 // where c is each coin in the coins array
 
 
@@ -9,9 +9,8 @@ class Solution {
     public int coinChange(int[] coins, int amount) {  
         int[] dp = new int[amount+1];   
         dp[0] = 0;
-        for(int i=1; i<amount+1; i++){
+        for(int i=1; i<amount+1; i++)
             dp[i] = amount+1;
-        }
 
         for (int i=1; i<amount+1; i++){
             for(int c : coins){
@@ -19,6 +18,7 @@ class Solution {
                     dp[i] = Math.min(dp[i], 1+dp[i-c]);
             }
         }
+
         if(dp[amount] != amount+1)
             return dp[amount];
         return -1;

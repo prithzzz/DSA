d7f6180478165da6bc5a4c7b3db7f61af796be6b
Prithika Soundararajan <prithikasoundararajan@gmail.com>
2026-07-26 11:16:41 +0530

    Time: 14 ms (5.21%) | Memory: 53.2 MB (10.56%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> result = new ArrayList<>();
+        result.add(""); //if u dont initiate result with empty str, then the list is empty and any new char is not appended to list
+        //If you started with zero words ([]), there would be nothing to append characters to. 
+
+        for(char c : s.toCharArray()){
+            List<String> temp = new ArrayList<>();
+
+            if (Character.isLetter(c)){
+                for(String curr : result){
+                    temp.add(curr + Character.toUpperCase(c));
+                    temp.add(curr + Character.toLowerCase(c));
+                }
+            }
+            else {
+                for(String curr : result){
+                    temp.add(curr + c);
+                }
+            }
+
+            result = temp;
+        }
+
+        return result;
+    }
+}

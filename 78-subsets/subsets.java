5be1ca79fbbd4b1eab43df460583950a54702dfc
Prithika Soundararajan <prithikasoundararajan@gmail.com>
2026-07-27 15:55:09 +0530

    Time: 1 ms (86.73%) | Memory: 44.1 MB (57.72%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,8 +1,3 @@
-// Approach: (Backtracking)
-// Since every element has two paths- either add or not add to result, leads to 
-// Time complexity - O(n.2^n)
-
-
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
@@ -14,14 +9,13 @@ class Solution {
 
     public void backtrack(int[] nums, int i, List<Integer> subset, List<List<Integer>> result){
 
-        if(i >= nums.length){
+        if(i >= nums.length)
             result.add(new ArrayList<>(subset));
-        }
         else{
             subset.add(nums[i]);
             backtrack(nums, i+1, subset, result);
             subset.remove(subset.size() - 1);
             backtrack(nums, i+1, subset, result);
-        }
+        }       
     }
 }

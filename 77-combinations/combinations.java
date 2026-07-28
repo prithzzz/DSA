8b51a7c401281cfa00e21c703cabc7c9f85326aa
prithzzz <prithikasoundararajan@gmail.com>
2026-07-28 17:17:54 +0530

    combinations approach update

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,3 +1,6 @@
+// Approach 1: (for loop)
+// Better approach to prune unnecessary recursive calls
+
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> output = new ArrayList<>();
@@ -14,6 +17,34 @@ class Solution {
                 backtrack(i + 1, n, k, combo, output);
                 combo.remove(combo.size() - 1);
             }
+        }
+    }
+}
+
+
+// Aprroach 2: (include/exclude branch)
+// Check for 2 conditions: if individual combination size is k, and if all numbers till n have been iterated
+//Time: O(2^n)
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> output = new ArrayList<>();
+        List<Integer> combo = new ArrayList<>();
+        backtrack(1,n,k, combo, output);
+        return output;
+    }
+
+    public void backtrack(int i,int n, int k, List<Integer> combo, List<List<Integer>> output){
+        if(combo.size() == k)
+            output.add(new ArrayList<>(combo));
+        else if(i > n)
+            return;
+        else{
+            combo.add(i);
+            backtrack(i+1,n, k, combo, output);
+
+            combo.remove(combo.size()-1);
+            backtrack(i+1,n, k, combo, output);
         }
     }
 }

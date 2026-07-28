ddf8d5bf9cbfbda886f89ada7f891ea67777715e
Prithika Soundararajan <prithikasoundararajan@gmail.com>
2026-07-28 17:13:45 +0530

    Time: 15 ms (93.17%) | Memory: 98.1 MB (27.41%) - LeetSync

--- 77-combinations/combinations.java
+++ 77-combinations/combinations.java
@@ -1,22 +1,19 @@
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> output = new ArrayList<>();
-        List<Integer> combo = new ArrayList<>();
-        backtrack(1,n,k, combo, output);
+        backtrack(1,n,k, new ArrayList<>(), output);
         return output;
     }
 
-    public void backtrack(int i,int n, int k, List<Integer> combo, List<List<Integer>> output){
+    public void backtrack(int start,int n, int k, List<Integer> combo, List<List<Integer>> output){
         if(combo.size() == k)
             output.add(new ArrayList<>(combo));
-        else if(i > n)
-            return;
         else{
-            combo.add(i);
-            backtrack(i+1,n, k, combo, output);
-
-            combo.remove(combo.size()-1);
-            backtrack(i+1,n, k, combo, output);
+            for (int i = start; i <= n - (k - combo.size()) + 1; i++) {
+                combo.add(i);
+                backtrack(i + 1, n, k, combo, output);
+                combo.remove(combo.size() - 1);
+            }
         }
     }
 }

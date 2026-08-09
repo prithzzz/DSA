442f8929e4bb78c73c858c0b4041256448e61613
prithzzz <prithikasoundararajan@gmail.com>
2026-08-09 21:06:59 +0530

    queue update

--- Stack/225-implement-stack-using-queues/implement-stack-using-queues.java
+++ Stack/225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -5,21 +5,24 @@ class MyStack {
         stack = new LinkedList<>();
     }
     
+    // Time: O(n)
+    // Space: O(n)
     public void push(int x) {
         stack.offer(x);
-        int i = stack.size() - 1;
+        int i = stack.size() - 1; //initiate loop at bottom of queue
         while(i>0){
-            stack.offer(stack.poll());
-            i--;
+            stack.offer(stack.poll()); //move the element at front of queue to the last 
+            i--; //decrement the end/last postion until the newely offered(added) element is at the front/top of stack
         }        
     }
     
+    // Time O(1)
     public int pop() {
-        return stack.poll();
+        return stack.poll(); //queue remove - poll
     }
     
     public int top() {
-        return stack.peek();
+        return stack.peek(); 
     }
     
     public boolean empty() {
